9f8d45495d031fb5fed7f4031935a3694d4b7d7a
John Feras <jferas@ferasinfotech.com>
2024-08-14 13:23:22 -0400

    Added tracking of created franchisers in fund handler via ghost array for use by recall handler

--- test/handlers/FranchiserFactoryHandler.sol
+++ test/handlers/FranchiserFactoryHandler.sol
@@ -12,6 +12,9 @@ contract FranchiserFactoryHandler is Test {
     FranchiserFactory public factory;
     Franchiser public franchiser;
 
+    // Handler ghost array to contain all the funded franchisers created by handler_fund
+    Franchiser[] public fundedFranchisers;
+
     constructor(FranchiserFactory _factory) {
         factory = _factory;
         franchiser = new Franchiser(IVotingToken(address(factory.votingToken())));
@@ -26,6 +29,15 @@ contract FranchiserFactoryHandler is Test {
         return bound(_amount, 0, 100000e18);
     }
 
+    function _removeFranchiser(uint256 _index) public {
+        require(_index < fundedFranchisers.length, "Index out of bounds");
+
+        for (uint256 i = _index; i < fundedFranchisers.length - 1; i++) {
+            fundedFranchisers[i] = fundedFranchisers[i + 1];
+        }
+        fundedFranchisers.pop();
+    }
+
     function handler_fund(address _delegator, address _delegatee, uint256 _amount) external {
         vm.assume(_validActorAddress(_delegator));
         _amount = _boundAmount(_amount);
@@ -35,17 +47,18 @@ contract FranchiserFactoryHandler is Test {
         votingToken.approve(address(factory), _amount);
         franchiser = factory.fund(_delegatee, _amount);
         vm.stopPrank();
+        fundedFranchisers.push(franchiser);
     }
 
-    function handler_recall(address _delegator, address _delegatee, uint256 _amount) external {
-        vm.assume(_validActorAddress(_delegator));
-        _amount = _boundAmount(_amount);
-        VotingTokenConcrete votingToken = VotingTokenConcrete(address(factory.votingToken()));
-        votingToken.mint(_delegator, _amount);
-        vm.startPrank(_delegator);
-        votingToken.approve(address(factory), _amount);
-        franchiser = factory.fund(_delegatee, _amount);
+    function handler_recall(uint256 _fundedFranchiserIndex) external {
+        _fundedFranchiserIndex = bound(_fundedFranchiserIndex, 0, fundedFranchisers.length - 1);
+        Franchiser _fundedFranchiser = fundedFranchisers[_fundedFranchiserIndex];
+        address _delegatee = _fundedFranchiser.delegatee();
+        address _delegator = _fundedFranchiser.delegator();
+        vm.prank(_delegator);
         factory.recall(_delegatee, _delegator);
-        vm.stopPrank();
+
+        // remove the franchiser from the fundedFranchisers array
+        _removeFranchiser(_fundedFranchiserIndex);
     }
 }
